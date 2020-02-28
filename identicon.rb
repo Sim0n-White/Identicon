57d79c1802ea7fbd8dc1feccd16b0252e1dc06dd
Simon <semenb1999@gmail.com>
2020-02-29 01:17:07 +0300

    Добавлена функция по определению области, которая должна быть закрашена. Добавлена функция по использованию всех цветов rgb

--- identicon.rb
+++ identicon.rb
@@ -4,7 +4,7 @@ require 'digest/md5'
 ## Параметр path - строка пути, куда сохраняется png с идентиконом. Опциональный, по умолчанию сохраняется в текущую папку
 class Identicon
   attr_accessor :user_name, :path
-  Tobyte = Struct.new(:name, :byte_array, :color_byte, :grid)
+  Tobyte = Struct.new(:name, :byte_array, :color_byte, :grid, :markPoint)
   def initialize(user_name, path = __dir__)
     @user_name = user_name
     @path = path
@@ -13,46 +13,42 @@ class Identicon
   def generate
     identicon = arrInput(user_name)
     makeGrid(identicon)
+    findBox(identicon)
     MiniMagick::Tool::Convert.new do |i|
       i.size "250x250"
       i.gravity "center"
       i.xc "white"
-      i.caption "test"
-      i << "test_image.png"
+      i.caption "identicon"
+      i << "identicon.png"
     end
 
     MiniMagick::Tool::Convert.new do |k|
       k.size "50x50"
       k.gravity "center"
-      k.xc identicon.color_byte
-      k.caption "cube"
-      k << "cube.png"
+      k.xc "rgb(#{identicon.color_byte[0]}, #{identicon.color_byte[1]}, #{identicon.color_byte[2]})"
+      k.caption "part"
+      k << "part.png"
     end
 
     image = MiniMagick::Image.new("test_image.png")
     cube = MiniMagick::Image.new("cube.png")
-    sum = -50
-    5.times do
+    identicon.markPoint.length.times do |i|
       result = image.composite(cube) do |c|
-        sum = sum + 50
+        x = identicon.markPoint[i] % 5 * 50
+        y = identicon.markPoint[i] / 5 * 50
+        y = y.to_i
         c.compose "Over"
-        c.geometry "+#{sum}+100"
+        c.geometry "+#{x}+#{y}"
       end
-      result.write "test_image.png"
+      result.write "identicon.png"
     end
+
+
   end
 
   def arrInput(name)
     byte_array = Digest::MD5.hexdigest(name).chars.each_slice(2).map(&:join).map(&:hex)
     color = byte_array.slice(0,3)
-    case color = color.each_with_index.max[1]
-    when 0
-      color = "red"
-    when 1
-      color = "green"
-    when 2
-      color = "blue"
-    end
     return Tobyte.new(name, byte_array, color)
   end
 
@@ -61,15 +57,20 @@ class Identicon
     i = 0
     while (i <= identicon.byte_array.length) && (i+3 <= identicon.byte_array.length-1)
       fill = identicon.byte_array[i..i+3]
-      p fill
       fill[3] = fill[1]
       fill[4] = fill[0]
       grid.concat fill
       i+=3
-      p i
     end
     identicon.grid = grid
   end
 
-
+  def findBox(identicon)
+    identicon.markPoint = []
+    identicon.grid.length.times do |i|
+      if identicon.grid[i].odd?
+        identicon.markPoint << i
+      end
+    end
+  end
 end
--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 require_relative  'identicon'
 ## Параметр user_name - строка
 ## Параметр path - строка пути, куда сохраняется png с идентиконом. Опциональный, по умолчанию сохраняется в текущую папку
-user_name = "bart"
+user_name = "cxzsas"
 path = nil
 identicon = Identicon.new(user_name, path)
 identicon.generate # генерирует и сохраняет файл png
