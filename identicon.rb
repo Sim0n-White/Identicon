3ac8566defac1dd0926e00183b2c1f211218c9b9
Simon <semenb1999@gmail.com>
2020-02-28 22:35:23 +0300

    Изменен порядок выбора цвета

--- identicon.rb
+++ identicon.rb
@@ -4,7 +4,7 @@ require 'digest/md5'
 ## Параметр path - строка пути, куда сохраняется png с идентиконом. Опциональный, по умолчанию сохраняется в текущую папку
 class Identicon
   attr_accessor :user_name, :path
-  Tobyte = Struct.new(:name, :byte_array, :color_byte)
+  Tobyte = Struct.new(:name, :byte_array, :color_byte, :grid)
   def initialize(user_name, path = __dir__)
     @user_name = user_name
     @path = path
@@ -13,9 +13,7 @@ class Identicon
   def generate
     identicon = arrInput(user_name)
     p identicon.byte_array
-    p identicon.byte_array.length
-    p identicon.color_byte
-
+    makeGrid(identicon)
     MiniMagick::Tool::Convert.new do |i|
       i.size "250x250"
       i.gravity "center"
@@ -27,15 +25,27 @@ class Identicon
     MiniMagick::Tool::Convert.new do |k|
       k.size "50x50"
       k.gravity "center"
-      k.xc "green"
+      k.xc identicon.color_byte
       k.caption "cube"
       k << "cube.png"
+    end
+
+    image = MiniMagick::Image.new("test_image.png")
+    cube = MiniMagick::Image.new("cube.png")
+    sum = -50
+    5.times do
+      result = image.composite(cube) do |c|
+        sum = sum + 50
+        c.compose "Over"
+        c.geometry "+#{sum}+100"
+      end
+      result.write "test_image.png"
     end
   end
 
   def arrInput(name)
-    byte_array = Digest::MD5.hexdigest(name).bytes.to_a
-    byte_array = byte_array.slice(0,16)
+    byte_array = Digest::MD5.hexdigest(name).chars.each_slice(2).map(&:join)
+    byte_array = byte_array.map(&:hex)
     color = byte_array.slice(0,3)
     case color = color.each_with_index.max[1]
     when 0
@@ -46,6 +56,11 @@ class Identicon
       color = "blue"
     end
     return Tobyte.new(name, byte_array, color)
+  end
+
+  def makeGrid(identicon)
+    grid = []
+
   end
 
 
