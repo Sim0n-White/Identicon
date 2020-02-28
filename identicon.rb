baeed39685eb2f478e1b4b594d38c16d090bff7b
Simon <semenb1999@gmail.com>
2020-02-28 21:53:59 +0300

    Добавлена функция определения цвета рисунка оп имени пользователя

--- identicon.rb
+++ identicon.rb
@@ -1,14 +1,21 @@
 require 'mini_magick'
+require 'digest/md5'
 ## Параметр user_name - строка
 ## Параметр path - строка пути, куда сохраняется png с идентиконом. Опциональный, по умолчанию сохраняется в текущую папку
 class Identicon
   attr_accessor :user_name, :path
+  Tobyte = Struct.new(:name, :byte_array, :color_byte)
   def initialize(user_name, path = __dir__)
     @user_name = user_name
     @path = path
   end
 
   def generate
+    identicon = arrInput(user_name)
+    p identicon.byte_array
+    p identicon.byte_array.length
+    p identicon.color_byte
+
     MiniMagick::Tool::Convert.new do |i|
       i.size "250x250"
       i.gravity "center"
@@ -24,17 +31,22 @@ class Identicon
       k.caption "cube"
       k << "cube.png"
     end
-
-    image = MiniMagick::Image.new("test_image.png")
-    cube = MiniMagick::Image.new("cube.png")
-    sum = 0
-    3.times do
-      result = image.composite(cube) do |c|
-        sum = sum + 50
-        c.compose "Over"
-        c.geometry "+#{sum}+100"
-      end
-      result.write "test_image.png"
-    end
   end
+
+  def arrInput(name)
+    byte_array = Digest::MD5.hexdigest(name).bytes.to_a
+    byte_array = byte_array.slice(0,16)
+    color = byte_array.slice(0,3)
+    case color = color.each_with_index.max[1]
+    when 0
+      color = "red"
+    when 1
+      color = "green"
+    when 2
+      color = "blue"
+    end
+    return Tobyte.new(name, byte_array, color)
+  end
+
+
 end
